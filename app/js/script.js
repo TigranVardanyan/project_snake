26c53cc2d3f28e709d99ddea3272c9e11ff0bfaa
Tigran <tig.vardanyan.001@gmail.com>
2018-11-28 18:30:14 +0400

    А наш малыш то пошел

--- app/js/script.js
+++ app/js/script.js
@@ -22,6 +22,40 @@ for(var i = 0; i < snake_position.length; i++) { // от [0 , 5]
     snake_position_number[i] = +positionNumber; // добовляем в массив
     cells[snake_position_number[i]].classList.add('snake'); //каждому элементу массива добовляет класс снейк
 }
+var start = document.getElementById('start');
+start.addEventListener("click" , autoGo)
+
+function autoGo() {
+    setInterval(function() {
+    snake_position_first_X = snake_position[snake_position.length-1][0];
+    snake_position_first_Y = snake_position[snake_position.length-1][1];
+    right();
+    cells[snake_position_number[0]].classList.remove('snake');
+    snake_position.shift();
+    for(var i = 0; i < snake_position.length; i++) { // от [0 , 5] 
+        snake_positionX = snake_position[i][0];//на каждую итерацию выводит Х и У каждой части змеи
+        snake_positionY = snake_position[i][1];
+        var positionNumber = +xMax*(snake_positionY-1)+snake_positionX; // считает номеропозицию каждой части
+        snake_position_number[i] = +positionNumber; // добовляем в массив
+        cells[snake_position_number[i]].classList.add('snake'); //каждому элементу массива добовляет класс снейк
+
+    }
+    for(var i = 0; i < snake_position_number.length; i++) {
+        var k = 0;
+        var opt1 = snake_position_number[i];
+        for(var j = 0; j < snake_position_number.length; j++) {
+            var opt2 = snake_position_number[j];
+            if(opt1 == opt2) {
+                k++;
+            }
+        }   
+        if(k > 1) {
+            gameover();
+            break;
+        }
+    }
+    }, 1000)
+}
 
 function keyPress() {
     snake_position_first_X = snake_position[snake_position.length-1][0];
