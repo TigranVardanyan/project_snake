cc9ddf7a3897da6b07b620f26c3b1e566aab98c9
Tigran <tig.vardanyan.001@gmail.com>
2020-04-18 02:39:39 +0400

    Refresh project

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,101 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	del = require('del'),
-	autoprefixer = require('gulp-autoprefixer');
+var gulp       = require('gulp'), // Подключаем Gulp
+	sass         = require('gulp-sass'), //Подключаем Sass пакет,
+	browserSync  = require('browser-sync'), // Подключаем Browser Sync
+	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+	uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+	cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+	rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
-gulp.task('sass', function(){ // Создаем таск Sass
-    return gulp.src('app/sass/**/*.+(sass|scss)') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-        .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+gulp.task('sass', function() { // Создаем таск Sass
+	return gulp.src('app/sass/**/*.+(scss|sass)') // Берем источник
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'app' // Директория для сервера - app
-        },
-        notify: false // Отключаем уведомления
-    });
+	// browserSync({ // Выполняем browserSync
+	//     server: { // Определяем параметры сервера
+	//         baseDir: 'app/' // Директория для сервера - app
+	//     },
+	//     notify: false // Отключаем уведомления
+	// });
+	browserSync.init({
+		proxy: "localhost/homepage/4project/project_snake/app",  //enter location
+		port: 433
+	});
 });
 
-gulp.task('clean', function() {
-    return del.sync('dist'); // Удаляем папку dist перед сборкой
+gulp.task('scripts', function() {
+	return gulp.src([ // Берем все необходимые библиотеки
+		'app/libs/**/*', // Берем js
+	])
+		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+		//.pipe(uglify()) // Сжимаем JS файл
+		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('app/sass/**/*.+(sass|scss)', ['sass']); // Наблюдение за sass файлами
-    // Наблюдение за другими типами файлов
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+		.pipe(browserSync.reload({ stream: true }))
 });
-gulp.task('build' , ['clean' , 'sass'], function() {
-	var buildCss = gulp.src([
-		'app/css/main.css'
-		])
-		.pipe(gulp.dest('dist/css'));
-	})
-	var buildjs = gulp.src([
-		'app/js/script.js'])
-		.pipe(gulp.dest('dist/js'));
-	var buildHtml = gulp.src([
-		'app/*.html'])
-		.pipe(gulp.dest('dist/'));
-	var buildPhp = gulp.src([
-		'app/*.php'])
-		.pipe(gulp.dest('dist/'));
-	var logic = gulp.src([
-		'app/logic/**/*.*'])
-		.pipe(gulp.dest('dist/logic'));
-	var img = gulp.src([
-		'app/img/**/*.*'])
-		.pipe(gulp.dest('dist/img'));
+
+gulp.task('css-libs', function() {
+	return gulp.src('app/css/libs.+(scss|sass)') // Выбираем файл для минификации
+		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(cssnano()) // Сжимаем
+		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+		.pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('clean', async function() {
+	return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({ // С кешированием
+			// .pipe(imagemin({ // Сжимаем изображения без кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))/**/)
+		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('prebuild', async function() {
+
+	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+		'app/css/main.css',
+	])
+		.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+		.pipe(gulp.dest('dist/fonts'))
+
+	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+		.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+		.pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.+(scss|sass)', gulp.parallel('sass')); // Наблюдение за sass файлами
+	gulp.watch('app/*.+(html|php)', gulp.parallel('code')); // Наблюдение за HTML и PHP файлами в корне проекта
+	gulp.watch(['app/js/*.js', 'app/libs/**/*.js'], gulp.parallel('scripts')); // Наблюдение за главным JS файлом и за библиотеками
+});
+gulp.task('default', gulp.parallel('css-libs', 'sass', 'scripts', 'browser-sync', 'watch'));
+gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'sass', 'scripts'));
